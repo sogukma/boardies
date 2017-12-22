69361131ac043ba22a3f627212d7789d32d9a436
Necati <necati.van@bluewin.ch>
2017-12-22 16:56:00 +0100

    Javadoc und änderungen

--- src/chat/ChatClient.java
+++ src/chat/ChatClient.java
@@ -5,13 +5,15 @@
 *
 * @author  Van Necati
 * @version 1.0
-* @since   2017-12-21 
+* @since   2017-12-22 
 */
 
 package chat;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -20,128 +22,149 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.StringReader;
 import java.net.Socket;
-
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JButton;
-import javax.swing.JEditorPane;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.html.HTMLEditorKit;
-
 import backend.BoardModel;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
 
 public class ChatClient {
 
 	private BoardModel BM1;
-	JFrame clientFrame;
-	JPanel clientPanel;
-	JTextArea textArea_Messages;
-	JTextField textField_ClientMessage;
-	JButton button_SendMessage;
-	JTextField textField_Username;
-	JScrollPane scrollPane_Messages;
-	// private JLabel UserName= new JLabel(BM1.getName().toString());
-	// private JLabel UserName = new JLabel("Test");
+	
+	JFrame frame;
+	JPanel panel;
+	JTextArea area;
+	JTextField field, name; 
+	JButton sendB, okB;
+	JScrollPane scroll;
 
-	Socket client;
-	PrintWriter writer;
-	BufferedReader reader;
+	Socket clients;
+	PrintWriter print;
+	BufferedReader buffer;
 
 	public static void main(String[] args) {
 		ChatClient c = new ChatClient();
-		c.createGUI();
+		c.GUI();
 	}
-
-	public void createGUI() {
-		clientFrame = new JFrame("Chat");
-		clientFrame.setSize(420, 340);
-
+	/**
+	 * GUI und serverConnect() wird ausgeführt und ein boolean
+	 * wird zurückgegeben.
+	 */
+	public void GUI() {
+		frame = new JFrame("DOMINION - Chat");
+		frame.setSize(600, 530);
+		frame.getContentPane().setBackground(Color.BLACK);
+		frame.setResizable(false);
+		
 		// Panel erzeugen, welches alle anderen Inhalte enthï¿½lt
-		clientPanel = new JPanel();
+		panel = new JPanel();
 
-		textArea_Messages = new JTextArea();
-		textArea_Messages.setEditable(false);
+		area = new JTextArea();
+		area.setBackground(Color.gray);
+		area.setEditable(false);
+		
+		name = new JTextField(8);		
+		
+		scroll = new JScrollPane(area);
+		scroll.setPreferredSize(new Dimension(500, 400));
+		scroll.setMinimumSize(new Dimension(500, 400));
+		scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+		
+		field = new JTextField(35);
+		field.addKeyListener(new enterSend());
 
-		textField_ClientMessage = new JTextField(17);
-		textField_ClientMessage.addKeyListener(new SendPressEnterListener());
+		sendB = new JButton("Send");
+		okB = new JButton();
+		sendB.addActionListener(new buttonSending());
+		
+		Font font1 = new Font("Verdana", Font.BOLD, 14);
+		area.setForeground(Color.orange);
+		area.setFont(font1);
+		Font font2 = new Font("Helvetica", Font.BOLD, 12);
+		name.setForeground(Color.red);
+		name.setFont(font2);
+		Font font3 = new Font("Helvetica", Font.PLAIN, 12);
+		field.setForeground(Color.black);
+		field.setFont(font3);
+				
+		panel.add(scroll);
+		panel.add(name, BorderLayout.PAGE_END);
+		panel.add(field);
+		panel.add(sendB);
+		panel.add(okB);
 
-		button_SendMessage = new JButton("Send");
-		button_SendMessage.addActionListener(new SendButtonListener());
-
-		textField_Username = new JTextField(6);
-		// UserName.setText(BM1.getName());
-		// UserName.setText("Halil");
-
-		// Scrollbalken zur textArea hinzufï¿½gen
-		scrollPane_Messages = new JScrollPane(textArea_Messages);
-		scrollPane_Messages.setPreferredSize(new Dimension(350, 250));
-		scrollPane_Messages.setMinimumSize(new Dimension(350, 250));
-		scrollPane_Messages.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane_Messages.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-		if (!connectToServer()) {
-			// Connect-Label anzeigen ob verbunden oder nicht...
+		frame.getContentPane().add(BorderLayout.CENTER, panel);
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame.setVisible(true);
+		
+		if(!serverConnect()){
+			okB.setText("Not Connected");				
+		}else{
+			okB.setText("Connected with Server");
 		}
-
-		Thread t = new Thread(new MessagesFromServerListener());
-		t.start();
-
-		clientPanel.add(scrollPane_Messages);
-		clientPanel.add(textField_Username, BorderLayout.PAGE_END);
-		clientPanel.add(textField_ClientMessage);
-		clientPanel.add(button_SendMessage);
-
-		// Panel zum ContentPane (Inhaltsbereich) hinzufï¿½gen
-		clientFrame.getContentPane().add(BorderLayout.CENTER, clientPanel);
-		// clientPanel.setAlignment(Pos.BOTTOM_CENTER);
-		clientFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		clientFrame.setVisible(true);
+		
+		Thread thread = new Thread(new serverMessages());
+		thread.start();
 	}
 
-	public boolean connectToServer() {
+	/**
+	 * Verbindung zum Server
+	 * @param Socket wird definiert
+	 * @param BuffredReader wird angelegt. Clients InputStream wird gebraucht
+	 * @param Outputstream des Clients wird auch gebraucht: PrintWriter wurde angelegt.
+	 * @param Anzeige in der TextArea.
+	 */
+	public boolean serverConnect() {
 		try {
-			client = new Socket("127.0.0.1", 5555);
-			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			writer = new PrintWriter(client.getOutputStream());
-			appendTextMessages("Netzwerkverbindung hergestellt");
+			clients = new Socket("localhost", 5555);
+			buffer = new BufferedReader(new InputStreamReader(clients.getInputStream()));
+			print = new PrintWriter(clients.getOutputStream());
+			messageToArea("Enter your name in the left field!"+"\n");
 
 			return true;
 		} catch (Exception e) {
-			appendTextMessages("Netzwerkverbindung konnte nicht hergestellt werden");
+			messageToArea("Network connection could not be etablished");
 			e.printStackTrace();
 
 			return false;
 		}
 	}
+	/**
+	 * Die Nachrichten werden dem Server übergeben.
+	 */
+	public void sendingToServer() {
+		DateFormat dFormat = new SimpleDateFormat("dd.MM.yyy  HH:mm");
+		Date d = new Date();															
+        
+		print.println(name.getText() + ":  " + field.getText()+
+				"   -   "+dFormat.format(d));
+		print.flush();
 
-	public void sendMessageToServer() {
-		writer.println(textField_Username.getText() + ": " + textField_ClientMessage.getText());
-		writer.flush();
-
-		textField_ClientMessage.setText("");
-		textField_ClientMessage.requestFocus();
+		field.setText("");
 	}
 
-	public void appendTextMessages(String message) {
-		textArea_Messages.append(message + "\n");
+	public void messageToArea(String message) {
+		area.append(message + "\n");
 	}
 
-	// Listener
-	public class SendPressEnterListener implements KeyListener {
+	/**
+	 * Beim Druecken der Enter-Taste, wird sendingToServer aufgerufen.
+	 */
+	public class enterSend implements KeyListener {
 
 		@Override
 		public void keyPressed(KeyEvent arg0) {
 			if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
-				sendMessageToServer();
+				sendingToServer();
 			}
 		}
 
@@ -152,35 +175,39 @@ public class ChatClient {
 		@Override
 		public void keyTyped(KeyEvent arg0) {
 		}
-
 	}
-
-	public class SendButtonListener implements ActionListener {
+	
+	/**
+	 * Bei Button-Klick, wird sendingToServer aufgerufen.
+	 */
+	public class buttonSending implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			sendMessageToServer();
+			sendingToServer();
 		}
-
 	}
-
-	public class MessagesFromServerListener implements Runnable {
+	
+	/**
+	 * Der Thread des Clients prüft stets, ob Nachrichten
+	 * vom Server angekommen sind.
+	 * 
+	 */
+	public class serverMessages implements Runnable {
 
 		@Override
 		public void run() {
 			String message;
 
 			try {
-				while ((message = reader.readLine()) != null) {
-					appendTextMessages(message);
-					textArea_Messages.setCaretPosition(textArea_Messages.getText().length());
+				while ((message = buffer.readLine()) != null) {
+					messageToArea(message);
+					area.setCaretPosition(area.getText().length());
 				}
 			} catch (IOException e) {
-				appendTextMessages("Nachricht konnte nicht empfangen werden!");
+				messageToArea("Message could not sent");
 				e.printStackTrace();
 			}
 		}
-
 	}
-
 }
--- src/chat/ChatServer.java
+++ src/chat/ChatServer.java
@@ -4,120 +4,137 @@
 *
 * @author  Van Necati
 * @version 1.0
-* @since   2017-12-21 
+* @since   2017-12-22 
 */
+
 package chat;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import backend.BoardModel;
-
-public class ChatServer implements Runnable {
-
-	private BoardModel Bm;
-	ServerSocket server;
-	ArrayList<PrintWriter> list_clientWriter;
-
-	final int LEVEL_ERROR = 1;
-	final int LEVEL_NORMAL = 0;
-
+/**
+ * @author Necati
+ *
+ */
+public class ChatServer implements Runnable{
+	
+	ServerSocket serverSocket;
+	ArrayList<PrintWriter> clientList;
+	
+	final int NOT = 1;
+	final int OKAY = 0;
+	
 	@Override
-	public void run() {
-		// ChatServer s = new ChatServer();
-		if (runServer()) {
-			listenToClients();
-		} else {
-			// do nothing
+	public void run(){
+		
+		if(serverStart()){
+		lookingforClients();
 		}
 	}
-
-	public class ClientHandler implements Runnable {
-
-		Socket client;
-		BufferedReader reader;
-
-		public ClientHandler(Socket client) {
-			try {
-				this.client = client;
-				reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			} catch (IOException e) {
-				// e.printStackTrace();
+	
+	/**
+	 * Server wird gestartet und läuft
+	 * @param kann nun die PrintWriter der Clients in die ArrayListe speichern
+	 */
+	private boolean serverStart() {
+		try{
+			serverSocket = new ServerSocket(5555);
+			String s1 = "Server has startet";
+			consoleText(s1, OKAY);
+			clientList = new ArrayList<PrintWriter>();
+			
+			return true;
+		} catch (Exception e){
+			String s0 = "Server failed to start";
+			consoleText(s0, NOT);
+			
+			return false;
+		}
+	}
+	
+	private void consoleText(String text, int ok_not) {
+		if(ok_not == OKAY){
+			System.out.println(text + "\n");
+		}else{
+			System.err.println(text + "\n");
+		}
+	}
+	
+	/**
+	 * Die beiden clients werden aufgenommen.
+	 * @param clients client, der sich beim serverSocket meldet und von ihm akzeptiert wird, wird gespeichert.
+	 * @param print Output vom Client wird empfangen.
+	 * @param clientList print von vorhin wird die ArrayList gespeichert; alle PrintWriter der CLients.
+	 * @param tClient für jeden aufgenommenen Client wird ein Thread gestartet.
+	 */
+	public void lookingforClients(){
+		while(true){
+			try{
+				Socket clients = serverSocket.accept();
+				PrintWriter print = new PrintWriter(clients.getOutputStream());
+				clientList.add(print);
+				
+				Thread tCLient = new Thread(new Handler(clients));
+				tCLient.start();
+			}catch (Exception e){
+				e.printStackTrace();
 			}
 		}
+	}
+	
+	/**
+	 * Für jeden Client, der aufgenommen wurde, wird ein Thread gestartet.
+	 * @param clients global. Uebergebener client wird gespeichert
+	 * @param bufferedReader der BufferedReader des clients.
+	 */
+	public class Handler implements Runnable{
+		Socket clients;
+		BufferedReader bufferedReader;
 
+		public Handler(Socket clients) {
+			try{
+				this.clients = clients;
+				bufferedReader = new BufferedReader(new InputStreamReader(clients.getInputStream()));
+			}catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		/**
+		 * Die Nachrichten der Clients werden in der Konsole angezeigt. 
+		 */
 		@Override
 		public void run() {
 			String message;
-
-			try {
-				while ((message = reader.readLine()) != null) {
-					appendTextToConsole("From client: \n" + message, LEVEL_NORMAL);
-					sendToAllClients(message);
+			String fromClient = "Client: " ;
+			
+			try{
+				while((message = bufferedReader.readLine()) != null){
+					consoleText(fromClient+"\n" + message, OKAY);
+					toClients(message);
 				}
-			} catch (IOException e) {
-				// e.printStackTrace();
-			}
-
+			}catch (Exception e){
+				e.printStackTrace();
+			}	
 		}
 	}
-
-	private void listenToClients() {
-		while (true) { // lï¿½uft die ganze Zeit
-			try {
-				Socket client = server.accept();
-
-				PrintWriter writer = new PrintWriter(client.getOutputStream());
-				list_clientWriter.add(writer);
-
-				Thread clientThread = new Thread(new ClientHandler(client));
-				clientThread.start();
-			} catch (IOException e) {
-				// e.printStackTrace();
-			}
-		}
-
-	}
-
-	private boolean runServer() {
-		try {
-			server = new ServerSocket(5555);
-			appendTextToConsole("sever has started", LEVEL_ERROR);
-			list_clientWriter = new ArrayList<PrintWriter>();
-			return true;
-		} catch (IOException e) {
-			appendTextToConsole("server couldn't start", LEVEL_ERROR);
-			// e.printStackTrace();
-			return false;
-		}
-
-	}
-
-	private void appendTextToConsole(String message, int level) {
-		if (level == LEVEL_ERROR) {
-			System.err.println(message + "\n");
-		} else {
-			System.out.println(message + "\n");
-		}
-	}
-
-	public void sendToAllClients(String mesage) {
-		Iterator it = list_clientWriter.iterator();
-
-		while (it.hasNext()) {
-			PrintWriter writer = (PrintWriter) it.next();
-			writer.println(mesage);
-			writer.flush();
+	
+	/**
+	 * Der Iterator geht durch die Arraylist durch. 
+	 * @param note Die Nachrichten aus der Liste werden gespeichert. 
+	 */
+	public void toClients(String note){
+		Iterator list = clientList.iterator();
+		
+		while(list.hasNext()){
+			PrintWriter print = (PrintWriter) list.next();
+			print.println(note);
+			print.flush();
 		}
 	}
 
